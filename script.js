fe946dbd5b4b37e3df042da5c9756cc3cb1509f3
MagicCode2000 <147378149+MagicCode2000@users.noreply.github.com>
2024-08-07 06:14:40 +0330

    Animated-website-28

---
+++ script.js
@@ -0,0 +1,2 @@
+document.querySelector(".stars").style.setProperty("--width", window.innerHeight * 1.4 + "px");
+document.querySelector(".stars").style.setProperty("--height", window.innerHeight + "px");
